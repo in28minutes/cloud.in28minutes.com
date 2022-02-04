f0bcbb76e02489a6d3879feb7f360cf27bf67b4e
@technology_enthusiast <46935197+beena-yatin-kanyal@users.noreply.github.com>
2022-02-04 17:23:10 +0530

    removing the code

--- projects/google-cloud-content/cloudrunemulator/infra/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-source ./config.sh
-
-echo "Starting cloud-run emulator on localhost environment"
-gcloud beta code dev "$SRV_CONFIG_FILE" --source="$APP_SOURCE_PATH" --dockerfile="$DOCKERFILE" --local-port="$CLOUD_RUN_EMULATOR_APP_PORT" --application-default-credential
--- projects/google-cloud-content/cloudrunemulator/infra/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# TO STOP THE EMULATOR PRESS Control+C
-# AFTER THE EMULATOR RUN THE BELOW COMMAND TO REMOVE THE ENV. VARIABLES
-echo "Cleaning up cloud-run emulator environment variables from localhost environment"
-unset SRV_CONFIG_FILE
-unset APP_SOURCE_PATH
-unset DOCKERFILE
-unset CLOUD_RUN_EMULATOR_APP_PORT
